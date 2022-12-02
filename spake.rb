72c09a1aa33731db269ed9caa636468064c6807b
Kiran Singh <ssnarik@gmail.com>
2022-12-01 22:29:52 -0800

    Update spake.rb

--- spake.rb
+++ spake.rb
@@ -4,29 +4,27 @@
 class Spake < Formula
   desc "Spake CLI tool for automatic machine translation."
   homepage "https://spake.uncommon.industries"
-  url "https://github.com/UncommonIndustries/spake-cli/releases/download/v0.1.2/spake-cli_v0.1.2_macos-arm64.tar.gz"
   version "v0.1.2"
-  sha256 "39acb3cbdfa6fa9ade77bc550684ad21a1f21da9a4bb4c6c6e226154e0a5edfc"
+  depends_on :macos
+  
   license "Apache-2.0"
+  
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/UncommonIndustries/spake-cli/releases/download/v0.1.2/spake-cli_v0.1.2_macos-arm64.tar.gz"
+      sha256 "39acb3cbdfa6fa9ade77bc550684ad21a1f21da9a4bb4c6c6e226154e0a5edfc"
 
-
-
-  def install
-
-    bin.install "spake-cli"
-
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test spake-cli`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+      def install
+        bin.install "spake-cli"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/UncommonIndustries/spake-cli/releases/download/v0.1.2/spake-cli_v0.1.2_macos-x86_64.tar.gz"
+      sha256 "d97047a5036f67c818c4955a56c558907eddf5057344adcb9a5aa543236bd028"
+      def install
+        bin.install "spake-cli"
+      end
+    end
+    
   end
 end
