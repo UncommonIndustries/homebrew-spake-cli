d2317f988a9ad73b4f257770b8d8bb126b83da1f
UncommonIndustries <feedback@uncommon.industries>
2023-01-29 19:45:33 +0000

    Update formula for new release.

--- spake.rb
+++ spake.rb
@@ -3,23 +3,23 @@
 class Spake < Formula
   desc "Spake CLI tool for automatic machine translation."
   homepage "https://spake.uncommon.industries"
-  version "v0.1.8"
+  version "v0.1.9"
   depends_on :macos
   
   license "Apache-2.0"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/UncommonIndustries/spake-cli/releases/download/v0.1.8/spake-cli_v0.1.8_macos-arm64.tar.gz"
-      sha256 "a5b6ff0bb2707c0c204453dd968f3d806fa585cb28a4e98dbfe02acfd7dfecec"
+      url "https://github.com/UncommonIndustries/spake-cli/releases/download/v0.1.9/spake-cli_v0.1.9_macos-arm64.tar.gz"
+      sha256 "0df011a58d34382c7f651fbba290912adc9f419ffa6e7e70ed0499fc224b61cd"
 
       def install
         bin.install "spake-cli"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/UncommonIndustries/spake-cli/releases/download/v0.1.8/spake-cli_v0.1.8_macos-x86_64.tar.gz"
-      sha256 "3525fe9969bbabf0db890cd5f56119fc1798e9a2c86ed13de3cc0714db243817"
+      url "https://github.com/UncommonIndustries/spake-cli/releases/download/v0.1.9/spake-cli_v0.1.9_macos-x86_64.tar.gz"
+      sha256 "6b43bdaba6b8f4ca47d3a857dfa7452e7703c33385761065de9a333397d75b0e"
       def install
         bin.install "spake-cli"
       end
